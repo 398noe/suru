b00fcc09aec5f5bc1641630f66f3122783dc07e6
Sakyuba Noe <398noe@gmail.com>
2023-01-16 04:18:48 +0900

    Delete /runtime from import path

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import "regenerator-runtime/runtime";
+import "regenerator-runtime";
 import { useEffect, useState } from "react";
 import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
 import { MdOutlineMic } from "react-icons/md";
