a6d29d5529158a674b890c37752905e74d8df53f
Sakyuba Noe <398noe@gmail.com>
2023-01-16 01:17:59 +0900

    Add button with mic icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
-import { Inter } from '@next/font/google'
+import { useState } from "react";
+import { MdOutlineMic } from "react-icons/md";
 
-const inter = Inter({ subsets: ['latin'] })
+export default function Index() {
+	const [text, setText] = useState<string>("");
 
-export default function Home() {
+	
+	
 	return (
-		<div>
-			
+		<div className="h-screen w-screen flex justify-center items-center">
+			<div className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+				<MdOutlineMic className="w-8 h-8"/>
+				<span className="sr-only">Icon description</span>
+			</div>
 		</div>
 	)
 }
